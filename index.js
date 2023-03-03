ffb6efbab33af9fb979c988a227fe47979627ac5
devmdtanvir <officialtanvir@hotmail.com>
2023-03-03 19:27:53 +0600

    I am solve two tricky problem solving

--- index.js
+++ index.js
@@ -9,3 +9,34 @@ for (let i = 0; i < array.length; i++) {
   }
 }
 console.log(largestNumber);
+
+// problem solving chatGPT ask questions and i am solution
+
+// find evan number an array
+
+const even = (numbers) => {
+  return numbers.filter((number) => {
+    return number % 2 === 0;
+  });
+};
+
+console.log(even([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
+
+// word first letter if lower convert uppercase
+
+const capitalize = (string) => {
+  let words = string.split(" ");
+  let capitalizedWords = [];
+
+  for (let i = 0; i < words.length; i++) {
+    let word = words[i];
+    let firstLetter = word[0].toUpperCase();
+    let restOfWord = word.slice(1);
+    let capitalizedWord = firstLetter + restOfWord;
+    capitalizedWords.push(capitalizedWord);
+  }
+
+  return capitalizedWords.join(" ");
+};
+
+console.log(capitalize("hello world")); // Hello World
