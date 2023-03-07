fc4be7d655ca1857a7c79d8f2f7b4522f593ca36
devmdtanvir <officialtanvir@hotmail.com>
2023-03-07 08:18:35 +0600

    javascrit ticky concepts is recap

--- vowal.js
+++ vowal.js
@@ -1,1 +1,22 @@
 // write an function ths function remove word vowel word remove
+/*
+Write a function that takes a string as an argument and returns a new string with all the vowels removed. Assume that the input string contains only lowercase letters.
+
+Here's an example:
+
+Input: "hello world"
+Output: "hll wrld"
+
+Can you give it a try?
+
+*/
+const vowels = ["a", "e", "i", "o", "u"];
+
+function removeVowels(letter) {
+  const splitLetters = letter.split(" ");
+  for (let i = 0; i < splitLetters.length; i++) {
+    let splitLetter = splitLetters[i];
+  }
+}
+
+removeVowels("Tanvir Hossain");
